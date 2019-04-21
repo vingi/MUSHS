2038e2d197f5e4c13c3f5bbc3e6ffb153d642ad3
vingi <vingi520@msn.com>
2019-04-21 22:33:38 +0800

    Upd: 更新任务惩罚期, 先去放弃last job, 后再进入惩罚检测

---
+++ lua/role/pinkmoon.lua
@@ -0,0 +1,59 @@
+--
+-- vingi.lua
+--
+-- ----------------------------------------------------------
+-- return-code checker for MUSHclient functions that return error codes
+-- ----------------------------------------------------------
+--
+--[[
+
+Call for those MUSHclient functions that return a result code (like eOK).
+Not all functions return such a code.
+
+eg.
+
+require "check
+  check (SetVariable ("abc", "def"))  --> works ok
+  check (SetVariable ("abc-", "def")) --> The name of this object is invalid
+
+--]]
+
+
+module("pinkmoon", package.seeall)
+
+settings= {
+    -- 是否自动向韦小宝买龙泉剑 (若设定为true, 则运行start后会先去买龙泉剑,该过程可能会很久,有可能几个小时才能买到, 买到后才会开始正常job)
+    AutoBuy_Xiaobao_Longquan = false,
+
+    -- 是否每天自动洪七公作菜10次
+    Auto_hqgzc_10times = false,
+
+    -- 是否 作任务之前 先double内力
+    CheckNeili_InAdvance = false,
+
+    -- 当打架无内力时(吃药在CD状态),执行的加内动作(无则可不填), 一些门派有恢复内力的内功, 如华山有yun zixia
+    Recover_neili = "",
+
+    -- 是否作华山2任务
+    HuashanJob_Step2 = true,
+
+    -- 是否自动蝶梦楼刷分
+    AutoDML = true,
+
+    -- 是否自动论坛收矿石
+    -- 说明, 自动收矿石会发送你的帐号密码至WEBAPI以调用收取矿石, 如果觉得不安全的, 请不要使用
+    AutoMine = true,
+
+    -- 是否每周四自动领取会员福利
+    AutoVipBenefit = true,
+
+    -- 是否自动吃满Ebook双倍经验书
+    AutoEbook = false,
+}
+
+
+function tt()
+    print("call vingi method success")
+end
+
+return pinkmoon 
